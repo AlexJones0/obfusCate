2ad7f6622bd11e61cbd6ed027e388b6d687a3734
xeovz <65985521+xeovz@users.noreply.github.com>
2022-10-30 15:55:21 +0000

    Check test directory exists before clearing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,10 +8,13 @@ import os
 
 def clean_dir():
     """A small utility to clean the testing directory, such that it can be reused."""
-    log_files = os.listdir(os.getcwd() + "/tests/testing")
+    test_path = os.getcwd() + "/tests/testing"
+    if not os.path.isdir(test_path):
+        return
+    log_files = os.listdir(test_path)
     for file_ in log_files:
         if file_.endswith(".txt"):
-            os.remove(os.path.join(os.getcwd() + "/tests/testing", file_))
+            os.remove(os.path.join(test_path, file_))
 
 
 class TestDebugFunctions(unittest.TestCase):
