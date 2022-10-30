e764f064005a78022746c18ee7d8c0024991a987
xeovz <65985521+xeovz@users.noreply.github.com>
2022-10-30 03:59:05 +0000

    Add transform pipeline cursor and documentation

--- app/cli.py
+++ app/cli.py
@@ -1,13 +1,18 @@
+""" File: cli.py
+Implements functions to implement the command-line interface of the program,
+such that it can be used through text interaction in a terminal window."""
+from select import select
 import sys
 from typing import Optional
 from .debug import print_error, create_log_file, log
-from .io import CSource
+from .io import CSource, menu_driven_option
 from .obfuscation import *
 from app import settings as cfg
 
 
 def help_menu() -> None:  # TODO change from 'python cli' to actual name
-    print("""################ CLI Help Manual ################
+    """Prints the help menu detailing usage of the CLI command interface."""
+    hstr = """################ CLI Help Manual ################
 This program takes as an argument some input C source program file and allows the application of a sequence of obfuscation transformations, resulting in an obfuscated C source file being produced. For more information on usage and options, see below.
 
 Usage: python cli input_c_file [output_file] [options]
@@ -16,67 +21,88 @@ Options:
     -h --help       | Displays this help menu.
     -l --noLogs     | Stops a log file being created for this execution.
     -s --seed x     | Initialises the program with the random seed x (some integer).
-""")
+"""
+    print(hstr)
 
 
-def get_transformations(source: CSource, seed: Optional[int] = None) -> Optional[CSource]:
-    new_source = source.copy()
-    transforms = []
+def get_transformations(
+    source: CSource, seed: Optional[int] = None
+) -> Optional[CSource]:
+    """Given a source program to obfuscate, this program implements the CLI that allows
+    users to select a sequence of obfuscation transformations to apply and obfuscates
+    the program, returning the result.
+
+    Args:
+        source (CSource): The C source code program to obfuscate.
+        seed (Optional[int], optional): The seed to use for randomness. Defaults to None.
+
+    Returns:
+        Optional[CSource]: If successful, returns the obfuscated C source code. If an
+        error occurs or the user quits, returns None.
+    """
+    selected_transforms = []
+    cursor_index = 0  # Cursor to allow traversal of transforms in CLI.
+
+    # Generate available transforms from implemented classes
     available_transforms = ObfuscationUnit.__subclasses__()
-    finished_choosing = False
-    while not finished_choosing:
-        # TODO maybe just keep a list somewhere instead?
-        print("\n")
-        for i, class_ in enumerate(available_transforms):
-            print(f" ({i+1}) {class_.name}: {class_.description}")
-        print(f" ({i+2}) Undo previous transform selection")
-        print(f" ({i+3}) Finish selecting transforms and continue\n")
-        print("Current transforms: {}\n\n >".format(", ".join(str(t) for t in transforms)), end="")
-        valid_input = False
-        while not valid_input:
-            try:
-                choice = input()
-                match choice:
-                    case 'q' | 'quit' | 'exit' | 'leave' | 'x':
-                        return None
-                    case 'back' | 'undo' | 'u':
-                        choice = len(available_transforms) + 1
-                    case 'done' | 'finish' | 'next' | 'continue':
-                        choice = len(available_transforms) + 2
-                    case _:
-                        choice = int(choice)
-                if choice > 0 and choice <= len(available_transforms):
-                    valid_input = True
-                elif choice == len(available_transforms) + 1:
-                    transforms = transforms[:-1]
-                    valid_input = True
-                    print(" >", end="")
-                elif choice == len(available_transforms) + 2:
-                    valid_input = True
-                    finished_choosing = True
-                else:
-                    print(
-                        "Invalid option choice. Please select a number corresponding to your choice, or type 'quit' to exit.\n >", end="")
-            except:
-                print("Invalid option choice. Please select a number corresponding to your choice, or type 'quit' to exit.\n >", end="")
-        if not finished_choosing and choice != len(available_transforms) + 1:
-            new_transform = available_transforms[choice-1].get_cli()
+    num_transforms = len(available_transforms)
+    options = [f"{t.name}: {t.description}" for t in available_transforms]
+    options.append("Move cursor left")
+    options.append("Move cursor right")
+    options.append("Remove transform after cursor")
+    options.append("Finish selecting transforms and continue")
+
+    # Iteratively get transform info until user selects to continue/quit.
+    done_selecting = False
+    while not done_selecting:
+        # Format current transforms with cursor location for printing
+        prompt = "\nCurrent transforms: {} >>> {}\n".format(
+            " -> ".join(str(t) for t in selected_transforms[:cursor_index]),
+            ""
+            if cursor_index == len(selected_transforms)
+            else (" -> ".join(str(t) for t in selected_transforms[cursor_index:])),
+        )
+        choice = menu_driven_option(options, prompt=prompt)
+        if choice == -1:  # Quit
+            return None
+        elif choice < num_transforms:  # Add a transform
+            new_transform = available_transforms[choice].get_cli()
             if new_transform is None:
                 return None
-            transforms.append(new_transform)
-    pipeline = Pipeline(seed, *transforms)
-    obfuscated = pipeline.process(source)
+            selected_transforms.append(new_transform)
+            cursor_index += 1  # Move cursor 1 right with new transform
+        elif choice == num_transforms:  # Move cursor left
+            cursor_index = max(cursor_index - 1, 0)
+        elif choice == num_transforms + 1:  # Move cursor right
+            cursor_index = min(cursor_index + 1, len(selected_transforms))
+        elif choice == num_transforms + 2:  # Delete transform after cursor
+            selected_transforms = (
+                selected_transforms[:cursor_index]
+                + selected_transforms[(cursor_index + 1) :]
+            )
+        else:  # Finished selecting
+            done_selecting = True
+
+    # Apply selected transform pipeline to given source code
+    obfuscated = Pipeline(seed, *selected_transforms).process(source)
     return obfuscated
 
 
-def run_cli() -> bool:
+def handle_CLI() -> bool:
+    """Handles the command line interface for the program, parsing command arguments
+    and options to determine which settings to apply and what other functionalities
+    and to call. Behaviour changes depending on the value of sys.argv, which depends
+    on the arguments supplied to the program when calling it.
+
+    Returns:
+        bool: Whether execution ended as expected or not (i.e. an error occurred).
+    """
     supplied_args = sys.argv
     if len(supplied_args) != 0 and supplied_args[0].endswith(".py"):
+        # Handle execution as python script rather than binary # TODO CHANGE?
         supplied_args = supplied_args[1:]
-    if len(supplied_args) == 0:
-        print_error(
-            "No source file was supplied. Use the -h or --help options for more information.")
-        return log("Aborting executions because no arguments were supplied.")
+
+    # Iteratively handle arguments/options
     args = []
     seed = None
     skipNext = False
@@ -85,40 +111,52 @@ def run_cli() -> bool:
             skipNext = False
             continue
         match arg:
-            case '-h' | '--help' | '--man' | '--manual':
+            case "-h" | "--help" | "--man" | "--manual":  # View help option
                 help_menu()
                 return True
-            case '-l' | '--noLogs':
+            case "-l" | "--noLogs":  # Disable logging option
                 cfg.LOGS_ENABLED = False
-            case '-s' | '--seed':
+            case "-s" | "--seed":  # Set random seed option
                 skipNext = True
-                if len(supplied_args) < (i+1):
+                if len(supplied_args) <= i:
                     print_error(
-                        "Some integer seed must be supplied with the -s and --seed options. Use the -h or --help options to see usage information.")
+                        "Some integer seed must be supplied with the -s and --seed options. Use the -h or --help options to see usage information."
+                    )
                     return False
                 try:
-                    seed = int(supplied_args[i+1])
+                    seed = int(supplied_args[i + 1])
                 except:
                     print_error(
-                        "Some integer seed must be supplied with the -s and --seed options. Use the -h or --help options to see usage information.")
+                        "Some integer seed must be supplied with the -s and --seed options. Use the -h or --help options to see usage information."
+                    )
                     return False
             case _:
-                if arg.startswith("-"):
+                if arg.startswith("-"):  # Handle unknown options
                     print_error(
-                        f"Unknown option '{arg}' supplied. Use the -h or --help options to see usage information.")
+                        f"Unknown option '{arg}' supplied. Use the -h or --help options to see usage information."
+                    )
                     return False
-                else:
+                else:  # Store valid arguments
                     args.append(arg)
+
+    # Setup logging information
     create_log_file()
     log(f"Began execution of CLI script.")
     log(f"Supplied arguments: {str(supplied_args)}")
-    if len(args) == 1:
+
+    # Determine subsequent execution based on number of supplied arguments.
+    if len(args) == 0:  # No arguments - error
+        print_error(
+            "No source file was supplied. Use the -h or --help options to see usage information."
+        )
+        log("Aborting executions because no arguments were supplied.")
+    elif len(args) == 1:  # 1 argument - take as input source file, and print output
         source = CSource(args[0])
         obfuscated = get_transformations(source, seed)
         if obfuscated is not None:
             print(obfuscated.contents)
         return True
-    elif len(args) == 2:
+    elif len(args) == 2:  # 2 arguments - input and output files
         source = CSource(args[0])
         obfuscated = get_transformations(source, seed)
         try:
--- app/io.py
+++ app/io.py
@@ -1,15 +1,34 @@
-from typing import Optional, TextIO
+""" File: io.py
+Implements classes and functions for handling input and output. """
+from typing import Iterable, Optional, Tuple, Union
 from .debug import print_error, log
 from clang.cindex import TranslationUnit, TranslationUnitLoadError
 import clang.cindex
 
-
+# Create the clang Index for parsing C source files.
 index = clang.cindex.Index.create()
 
 
-class CSource():
+class CSource:
+    """A class representing a C source code program, storing its contents in both
+    string form and as a Clang-parsed translation unit."""
 
-    def __init__(self, filepath: str, contents: Optional[str] = None, t_unit: Optional[TranslationUnit] = None):
+    def __init__(
+        self,
+        filepath: str,
+        contents: Optional[str] = None,
+        t_unit: Optional[TranslationUnit] = None,
+    ):
+        """A constructor for a CSource object.
+
+        Args:
+            filepath (str): The path to the C file.
+            contents (Optional[str], optional): The contents of the file. Defaults to
+            None, in which case the file path is opened and read to retrieve contents.
+            t_unit (Optional[TranslationUnit], optional): The translation unit of the file
+            after it is parsed by clang. Defaults to None, in which case it is retrieved
+            by parsing the file.
+        """
         self.fpath = filepath
         if contents is None:
             log(f"Started loading source file with path {filepath}")
@@ -17,19 +36,25 @@ class CSource():
         else:
             self.contents = contents
         if self.contents is None:
-            return 
+            return
         if t_unit is None:
             self.t_unit = self.parse()
         else:
             self.t_unit = t_unit
 
-    def __read_file(self) -> str:
+    def __read_file(self) -> Optional[str]:
+        """Reads the file contents from the stored file path, raising errors and logging
+        as appropriate.
+
+        Returns:
+            Optional[str]: The string contents of the file, or None if some error occurs.
+        """
         if not self.fpath.endswith(".c"):
             print_error(f"Supplied file {self.fpath} is not a .c file!")
             return None
         log("Started reading contents from source file.")
         try:
-            with open(self.fpath, 'r') as source_file:
+            with open(self.fpath, "r") as source_file:
                 contents = source_file.read()
             return contents
         except OSError as e:
@@ -37,6 +62,13 @@ class CSource():
         return None
 
     def parse(self) -> Optional[TranslationUnit]:
+        """Parses the file using the clang parser to produce a translation unit, raising
+        errors and logging as appropriate.
+
+        Returns:
+            Optional[TranslationUnit]: The clang translation unit of the parsed file, or
+            None if some error occurs.
+        """
         log("Attempting to parse program.")
         try:
             t_unit = index.parse(self.fpath)
@@ -44,14 +76,76 @@ class CSource():
         except TranslationUnitLoadError as e:
             log(f"Error parsing the program: {str(e)}")
             print_error(
-                f"An error occurred whilst trying to parse {self.fpath} - check that this compiles without any errors or warnings and try again.")
+                f"An error occurred whilst trying to parse {self.fpath} - check that this compiles without any errors or warnings and try again."
+            )
             return None
         except Exception as e:
             log(f"Unexpected error whilst parsing the program: {str(e)}.")
             print_error(
-                f"An unknown error occurred whilst trying to parse {self.fpath}.")
+                f"An unknown error occurred whilst trying to parse {self.fpath}."
+            )
             return None
 
-    def copy(self):
+    def copy(self) -> "CSource":
+        """Creates a copy of the code record, producing a separate translation unit.
+
+        Returns:
+            CSource: A copy of the file with the same path, contents and parsed contents.
+        """
         log(f"Creating copy of source: {self.fpath}.")
         return CSource(self.fpath, self.contents)
+
+
+def menu_driven_option(
+    options: Iterable[Union[str, Tuple[str, Iterable[str]]]], prompt: str = None
+) -> int:
+    """Performs command-line interface input handling for a generalized menu-driven
+    system in which a restricted integer input must be made to select one of many
+    options. Returns the chosen option.
+
+    Args:
+        options (Iterable[Union[str, Tuple[str, Iterable[str]]]]): The list of
+        options to provide. Each option can either be a string, or a tuple like
+        (string, [str1, str2, str3, ...]) where string is the option text and
+        [str1, str2, str3, ...] are optional strings that the CLI should understand
+        as a selction of that choice.
+        prompt (str, optional): Additional prompt text to display to the user after
+        the printing of options but before requesting input. Defaults to None.
+
+    Returns:
+        int: The integer index of the selected choice. -1 if it was selected to quit.
+    """
+    # Display options and prompt
+    for i, option in enumerate(options):
+        print(f" ({i+1}) {option}")
+    print(prompt + "\n >", end="")
+
+    # Loop until valid input received
+    valid_input = False
+    while not valid_input:
+        try:
+            choice = input().lower().strip()
+            # Check for quit inputs
+            if choice in ["q", "quit", "exit", "leave", "x"]:
+                return -1
+            # Check for keyword matches
+            for i, option in enumerate(options):
+                if isinstance(option, tuple) and choice in option[1]:
+                    choice = i
+                    break
+            # Check for valid integer choice ranges
+            if not isinstance(choice, int):
+                choice = int(choice)
+            if choice > 0 and choice <= len(options):
+                valid_input = True
+            else:
+                print(
+                    "Invalid option choice. Please select a number corresponding to your choice, or type 'quit' to exit.\n >",
+                    end="",
+                )
+        except: # Handle non-integer (and non-keyword) inputs
+            print(
+                "Invalid option choice. Please select a number corresponding to your choice, or type 'quit' to exit.\n >",
+                end="",
+            )
+    return choice - 1
