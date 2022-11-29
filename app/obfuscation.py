90a6f11ebb719164aff8f88c34cd66090dbb15d2
xeovz <65985521+xeovz@users.noreply.github.com>
2022-11-29 10:45:43 +0000

    Add minimise idents - not working yet

--- app/obfuscation.py
+++ app/obfuscation.py
@@ -154,16 +154,26 @@ class IdentifierTraverser(NodeVisitor):
         ONLY_UNDERSCORES = "Only underscores" # TODO will this break anything?
         MINIMAL_LENGTH   = "Minimal length"
 
-    def __init__(self, style: Style):
+    def __init__(self, style: Style, minimiseIdents : bool):
         self.idents = {"main": "main"}
-        self.new_idents = set()
+        self._new_idents = set()
+        self._scopes = list()
         self.style = style
-        self.pragmas = []
-        self._in_scope = {}
+        self.minimiseIdents = minimiseIdents
 
     def get_new_ident(self, ident):
+        if self.minimiseIdents: # TODO THIS OPTION IS VERY BROKE BUT COMPLEX SO JUST LEAVE IT FOR NOW?
+            for new_ident in self._new_idents:
+                in_scope = False # TODO maintain a list of unused idents - will be cleaner and cheaper
+                for scope in self._scopes[::-1]:
+                    if new_ident in scope:
+                        in_scope = True
+                        break
+                if not in_scope:
+                    self.idents[ident] = new_ident
+                    return new_ident
         new_ident = ""
-        while len(new_ident) == 0 or new_ident in self.new_idents:
+        while len(new_ident) == 0 or new_ident in self._new_idents:
             if self.style == self.Style.COMPLETE_RANDOM:
                 size_ = randint(4, 19)
                 new_ident = randchoice(ascii_letters)[0]
@@ -171,9 +181,9 @@ class IdentifierTraverser(NodeVisitor):
                     randchoice(ascii_letters + ascii_digits + "_" * 6, k=size_)
                 )
             elif self.style == self.Style.ONLY_UNDERSCORES:
-                new_ident = "_" * (len(self.new_idents) + 1)
+                new_ident = "_" * (len(self._new_idents) + 1)
             elif self.style == self.Style.MINIMAL_LENGTH:
-                cur_num = len(self.new_idents) + 1
+                cur_num = len(self._new_idents) + 1
                 #choices = "_" + ascii_letters + ascii_digits
                 choices = ascii_letters
                 new_ident = ""
@@ -183,7 +193,7 @@ class IdentifierTraverser(NodeVisitor):
                     cur_num = cur_num // len(choices)
                     if cur_num == 0:
                         break
-        self.new_idents.add(new_ident)
+        self._new_idents.add(new_ident)
         self.idents[ident] = new_ident
         return new_ident
 
@@ -192,6 +202,22 @@ class IdentifierTraverser(NodeVisitor):
             if node.name not in self.idents:
                 self.get_new_ident(node.name)
             node.name = self.idents[node.name]
+            self._scopes[-1].add(node.name)
+
+    def visit_FileAST(self, node):
+        self._scopes.append(set())
+        NodeVisitor.generic_visit(self, node)
+        self._scopes = self._scopes[:-1]
+    
+    def visit_FuncDef(self, node):
+        self._scopes.append(set())
+        NodeVisitor.generic_visit(self, node)
+        self._scopes = self._scopes[:-1]
+    
+    def visit_Compound(self, node):
+        self._scopes.append(set())
+        NodeVisitor.generic_visit(self, node)
+        self._scopes = self._scopes[:-1]
 
     def visit_Decl(self, node):
         self.scramble_ident(node)
@@ -204,6 +230,7 @@ class IdentifierTraverser(NodeVisitor):
     def visit_Enum(self, node):
         self.scramble_ident(node)
         NodeVisitor.generic_visit(self, node)
+        self._in_scope.remove(node.name)
     
     def visit_Enumerator(self, node):
         self.scramble_ident(node)
@@ -222,6 +249,7 @@ class IdentifierTraverser(NodeVisitor):
             if node.declname not in self.idents:
                 self.get_new_ident(node.declname)
             node.declname = self.idents[node.declname]
+            self._scopes[-1].add(node.declname)
         NodeVisitor.generic_visit(self, node)
 
     def visit_ID(self, node):
@@ -253,7 +281,79 @@ class IdentifierTraverser(NodeVisitor):
         debug.log("Could not continue obfuscation because the obfuscator cannot handle static asserts!")
         exit()
 
-    # TODO: ArrayRef, Enum, Enumerator, FuncCall, Goto, Label, NamedInitializer, Struct, StructRef
+
+class IdentitifierRenameUnit(ObfuscationUnit):
+    """Implements an identifier rename (IRN) obfuscation transformation, which takes the input
+    source code and renames all identifiers (function names, parameter names, variable names, etc.)
+    such that the program still performs the same functionality, but now the identifier names reveal
+    no meaningful information about the program and are difficult to humanly comprehend."""
+
+    name = "Identifier Renaming"
+    description = "Renames variable/function names to make them uncomprehensible."
+
+    def __init__(self, style, minimiseIdents):
+        self.style = style
+        self.minimiseIdents = minimiseIdents
+        self.traverser = IdentifierTraverser(style, minimiseIdents)
+
+    def transform(self, source: CSource) -> CSource:
+        self.traverser.visit(source.t_unit)
+        new_contents = generate_new_contents(source)
+        return CSource(source.fpath, new_contents, source.t_unit)
+
+    def edit_cli(self) -> bool:
+        options = [s.value for s in IdentifierTraverser.Style]
+        options.append("placeholder")
+        options.append("Finish editing")
+        while True:
+            prompt = f'\nChoose a style for the identifier renaming. Your current style is "{self.style.value}".\n'
+            if self.minimiseIdents:
+                options[len(IdentifierTraverser.Style)] = "Disable minimal identifier usage option (currently: ENABLED)"
+            else:
+                options[len(IdentifierTraverser.Style)] = "Enable minimal identifer usage option (currently: Disabled)"
+            choice = menu_driven_option(options, prompt)
+            if choice == -1:
+                return False
+            elif choice == len(IdentifierTraverser.Style):
+                self.minimiseIdents = not self.minimiseIdents
+                self.traverser.minimiseIdents = self.minimiseIdents
+            elif choice == len(options) - 1:
+                return True
+            else:
+                self.style = IdentifierTraverser.Style(options[choice])
+                self.traverser.style = self.style
+        return True
+
+    def get_cli() -> Optional["IdentitifierRenameUnit"]:
+        options = [s.value for s in IdentifierTraverser.Style]
+        prompt = "\nChoose a style for the identifier renaming.\n"
+        minimiseIdents = False
+        validChoice = False
+        while not validChoice:
+            if minimiseIdents:
+                options.append("Disable minimal identifier usage option (currently: ENABLED)")
+            else:
+                options.append("Enable minimal identifer usage option (currently: DISABLED)")
+            choice = menu_driven_option(options, prompt)
+            if choice == -1:
+                return None
+            elif choice == len(IdentifierTraverser.Style):
+                minimiseIdents = not minimiseIdents
+                options = options[:-1]
+            else:
+                style = IdentifierTraverser.Style(options[choice])
+                return IdentitifierRenameUnit(style, minimiseIdents)
+        return None
+
+    def __eq__(self, other: ObfuscationUnit) -> bool:
+        if not isinstance(other, IdentitifierRenameUnit):
+            return False
+        return self.style == other.style
+ 
+    def __str__(self):
+        style_flag = f"style={self.style.name}"
+        minimise_ident_flag = f"minimal={'ENABLED' if self.minimiseIdents else 'DISABLED'}"
+        return f"RenameIdentifiers({style_flag},{minimise_ident_flag})"
 
 
 class IdentitifierRenameUnit(ObfuscationUnit):
@@ -265,9 +365,10 @@ class IdentitifierRenameUnit(ObfuscationUnit):
     name = "Identifier Renaming"
     description = "Renames variable/function names to make them uncomprehensible."
 
-    def __init__(self, style):
+    def __init__(self, style, minimiseIdents):
         self.style = style
-        self.traverser = IdentifierTraverser(style)
+        self.minimiseIdents = minimiseIdents
+        self.traverser = IdentifierTraverser(style, minimiseIdents)
 
     def transform(self, source: CSource) -> CSource:
         self.traverser.visit(source.t_unit)
@@ -276,22 +377,47 @@ class IdentitifierRenameUnit(ObfuscationUnit):
 
     def edit_cli(self) -> bool:
         options = [s.value for s in IdentifierTraverser.Style]
-        prompt = f'\nChoose a style for the identifier renaming. Your current style is "{options[self.style-1]}".\n'
-        choice = menu_driven_option(options, prompt)
-        if choice == -1:
-            return False
-        self.style = IdentifierTraverser.Style(options[choice])
-        self.traverser.style = self.style
+        options.append("placeholder")
+        options.append("Finish editing")
+        while True:
+            prompt = f'\nChoose a style for the identifier renaming. Your current style is "{self.style.value}".\n'
+            if self.minimiseIdents:
+                options[len(IdentifierTraverser.Style)] = "Disable minimal identifier usage option (currently: ENABLED)"
+            else:
+                options[len(IdentifierTraverser.Style)] = "Enable minimal identifer usage option (currently: Disabled)"
+            choice = menu_driven_option(options, prompt)
+            if choice == -1:
+                return False
+            elif choice == len(IdentifierTraverser.Style):
+                self.minimiseIdents = not self.minimiseIdents
+                self.traverser.minimiseIdents = self.minimiseIdents
+            elif choice == len(options) - 1:
+                return True
+            else:
+                self.style = IdentifierTraverser.Style(options[choice])
+                self.traverser.style = self.style
         return True
 
     def get_cli() -> Optional["IdentitifierRenameUnit"]:
         options = [s.value for s in IdentifierTraverser.Style]
         prompt = "\nChoose a style for the identifier renaming.\n"
-        choice = menu_driven_option(options, prompt)
-        if choice == -1:
-            return None
-        style = IdentifierTraverser.Style(options[choice])
-        return IdentitifierRenameUnit(style)
+        minimiseIdents = False
+        validChoice = False
+        while not validChoice:
+            if minimiseIdents:
+                options.append("Disable minimal identifier usage option (currently: ENABLED)")
+            else:
+                options.append("Enable minimal identifer usage option (currently: DISABLED)")
+            choice = menu_driven_option(options, prompt)
+            if choice == -1:
+                return None
+            elif choice == len(IdentifierTraverser.Style):
+                minimiseIdents = not minimiseIdents
+                options = options[:-1]
+            else:
+                style = IdentifierTraverser.Style(options[choice])
+                return IdentitifierRenameUnit(style, minimiseIdents)
+        return None
 
     def __eq__(self, other: ObfuscationUnit) -> bool:
         if not isinstance(other, IdentitifierRenameUnit):
@@ -300,4 +426,6 @@ class IdentitifierRenameUnit(ObfuscationUnit):
  
     def __str__(self):
         style_flag = f"style={self.style.name}"
-        return f"RenameIdentifiers({style_flag})"
+        minimise_ident_flag = f"minimal={'ENABLED' if self.minimiseIdents else 'DISABLED'}"
+        return f"RenameIdentifiers({style_flag},{minimise_ident_flag})"
+
