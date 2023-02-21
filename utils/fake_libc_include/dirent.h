b6e5ea5ce1dcbf06a053c93675936675216ca24f
xeovz <65985521+xeovz@users.noreply.github.com>
2023-02-21 12:03:02 +0000

    Fix for dirent.h fake include header

--- utils/fake_libc_include/dirent.h
+++ utils/fake_libc_include/dirent.h
@@ -1,2 +1,27 @@
 #include "_fake_defines.h"
 #include "_fake_typedefs.h"
+
+typedef struct {
+    /* Always zero */
+    long d_ino;
+
+    /* File position within stream */
+    long d_off;
+
+    /* Structure size */
+    unsigned short d_reclen;
+
+    /* Length of name without \0 */
+    size_t d_namlen;
+
+    /* File type */
+    int d_type;
+
+    /* File name */
+    char d_name[PATH_MAX+1];
+} dirent;
+
+typedef struct {
+    struct dirent ent;
+    struct _WDIR *wdirp;
+} DIR;
