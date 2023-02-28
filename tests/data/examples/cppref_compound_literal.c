efa55fa04f53e964e6318cc9604553c808f54532
xeovz <65985521+xeovz@users.noreply.github.com>
2023-02-28 23:49:57 +0000

    Add obfuscation amenability recs + bugfix

--- tests/data/examples/cppref_compound_literal.c
+++ tests/data/examples/cppref_compound_literal.c
@@ -11,25 +11,6 @@ const float *pc = (const float []){1e0, 1e1, 1e2}; // read-only compound literal
  
 struct point {double x,y;};
  
-int main(void)
-{
-    int n = 2, *p = &n;
-    p = (int [2]){*p}; // creates an unnamed automatic array of type int[2]
-                       // initializes the first element to the value formerly
-                       // held in *p
-                       // initializes the second element to zero
-                       // stores the address of the first element in p
- 
-    void drawline1(struct point from, struct point to);
-    void drawline2(struct point *from, struct point *to);
-    drawline1(
-        (struct point){.x=1, .y=1},  // creates two structs with block scope and
-        (struct point){.x=3, .y=4}); // calls drawline1, passing them by value
-    drawline2(
-        &(struct point){.x=1, .y=1},  // creates two structs with block scope and
-        &(struct point){.x=3, .y=4}); // calls drawline2, passing their addresses
-}
- 
 void drawline1(struct point from, struct point to)
 {
     printf("drawline1: `from` @ {%.2f, %.2f}, `to` @ {%.2f, %.2f}\n",
@@ -40,4 +21,21 @@ void drawline2(struct point *from, struct point *to)
 {
     printf("drawline2: `from` @ {%.2f, %.2f}, `to` @ {%.2f, %.2f}\n",
         from->x, from->y, to->x, to->y);
+}
+
+int main(void)
+{
+    int n = 2, *p = &n;
+    p = (int [2]){*p}; // creates an unnamed automatic array of type int[2]
+                       // initializes the first element to the value formerly
+                       // held in *p
+                       // initializes the second element to zero
+                       // stores the address of the first element in p
+ 
+    drawline1(
+        (struct point){.x=1, .y=1},  // creates two structs with block scope and
+        (struct point){.x=3, .y=4}); // calls drawline1, passing them by value
+    drawline2(
+        &(struct point){.x=1, .y=1},  // creates two structs with block scope and
+        &(struct point){.x=3, .y=4}); // calls drawline2, passing their addresses
 }
