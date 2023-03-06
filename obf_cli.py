df80027f7d0c1c0f59eb200fce288670512a0b39
xeovz <65985521+xeovz@users.noreply.github.com>
2023-03-06 02:44:40 +0000

    Misc unnecessary files/imports

--- obf_cli.py
+++ obf_cli.py
@@ -1,5 +1,4 @@
 if __name__ == "__main__":
     import sys
-    import app 
     from app import cli
     sys.exit(cli.handle_CLI())
--- obf_gui.py
+++ obf_gui.py
@@ -1,5 +1,4 @@
 if __name__ == "__main__":
     import sys
-    import app 
     from app import gui
     sys.exit(gui.handle_gui())
