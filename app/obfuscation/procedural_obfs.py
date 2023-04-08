a3533c91934fa263ab505a340503e74d998010f1
xeovz <65985521+xeovz@users.noreply.github.com>
2023-04-08 14:57:12 +0100

    Clean up `app/obfuscation/procedural_obfs.py`

--- app/obfuscation/procedural_obfs.py
+++ app/obfuscation/procedural_obfs.py
@@ -9,19 +9,21 @@ from ..debug import *
 from .utils import ObfuscationUnit, TransformType, generate_new_contents
 from .identifier_analysis import IdentifierAnalyzer
 from pycparser.c_ast import *
-from typing import Optional
+from typing import Iterable
 import random, string, json
 
 
 class FuncArgRandomiserTraverser(NodeVisitor):
-    """TODO"""
+    """ Traverses an AST, finding function definitions and function specifications within
+    the AST, and performs function argument randomisation, inserting spurious arguments
+    and randomising the argument order depending on set options."""
 
-    # TODO add signed types in the future as well
-    # TODO get types defined in scope also? Like enums/structs/unions etc.
+    # Types and anonymous functions to generate their values
+    # for spurious argument creation
     types = {
-        "short": lambda: Constant("short", str(random.randint(-32767, 32767))),
-        "int": lambda: Constant("int", str(random.randint(-32767, 32767))),
-        "long": lambda: Constant("long", str(random.randint(-2147483647, 2147483647))),
+        "short": lambda: Constant("short", str(random.randint(-10, 10))),
+        "int": lambda: Constant("int", str(random.randint(-10, 10))),
+        "long": lambda: Constant("long", str(random.randint(-100, 100))),
         "long long": lambda: Constant(
             "long long", str(random.randint(-9223372036854775807, 9223372036854775807))
         ),
@@ -37,32 +39,53 @@ class FuncArgRandomiserTraverser(NodeVisitor):
                     )
                 )
             ),
-        ),  # TODO better random strings?
+        ),
         "_Bool": lambda: Constant("_Bool", str(random.choice([1, 0]))),
         "float": lambda: Constant(
-            "float", str(round(random.uniform(-10000000000, 10000000000), 4))
+            "float", str(random.choice([random.randint(-20, 20) * 0.25,
+                                        round(random.uniform(-10000000000, 10000000000), 4)]))
         ),
         "double": lambda: Constant(
-            "double", str(round(random.uniform(-10000000000000, 10000000000000), 6))
+            "double", str(random.choice([random.randint(-200, 200) * 0.125,
+                                        round(random.uniform(-10000000000, 10000000000), 4)]))
         ),
     }
 
     def __init__(self, extra: int, randomise: bool):
+        """The constructor for the FuncArgRandomiserTraverser. 
+
+        Args:
+            extra (int): The number of extra spurious arguments to insert
+            randomise (bool): Whether to randomise function argument orders or not.
+        """
         self.extra = extra
         self.randomise = randomise
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
+        """Reset the FuncArgRandomiserTraverser such that it can be used to traverse and
+        mutate another AST, resetting its state variables and internal data structures. """
         self.func_args = dict()
         self.walk_num = 1
         self.current_func = None
         self.analyzer = IdentifierAnalyzer()
 
-    def get_extra_args(self, idents):
+    def get_extra_args(self, idents: Iterable[str]) -> list[Decl]:
+        """Given a list of function parameter identifier names, this function generates
+        the required number of extra parameters, returning this is a list of declarations
+        to append to the end of the function signature parameter list. Created parameters
+        are ensured to be unique, and are of the form extra1, extra2, extra3, etc.
+
+        Args:
+            idents (Iterable[str]): The list of current parameter identifier names
+
+        Returns:
+            list[Decl]: The list of Decl subtrees for new spurious parameters. 
+        """
         extra_args = []
         basename = "extra"
         count = 0
-        for i in range(self.extra):
+        for _ in range(self.extra):
             argname = basename + str(count)
             count += 1
             while argname in idents:
@@ -70,23 +93,31 @@ class FuncArgRandomiserTraverser(NodeVisitor):
                 count += 1
             argtype = IdentifierType([random.choice(list(self.types.keys()))])
             typedecl = TypeDecl(argname, [], None, argtype)
-            # TODO support for pointer arguments in the future?
-            # rand = random.random()
-            # if rand < 0.4:
-            #    typedecl = PtrDecl([], typedecl)
-            #    if random < 0.075:
-            #        typedecl = PtrDecl([], typedecl)
             arg = Decl(argname, [], [], [], [], typedecl, None, None, None)
             extra_args.append(arg)
         return extra_args
 
-    def visit_FuncDef(self, node):
+    def visit_FuncDef(self, node: FuncDef) -> None:
+        """Visit a FuncDef AST node, updating the current_func attribute to reflect
+        the current function that is being parsed in the AST. """
         old_func = self.current_func
         self.current_func = node
         self.generic_visit(node)
         self.current_func = old_func
 
-    def get_fname(self, node): # TODO is this fix good enough?
+    def get_fname(self, node: Node) -> str | None:
+        """ Given an AST node corresponding to a function's type subtree, this function
+        attempts to retrieve the relevant function name from the subtree, iteratively 
+        parsing through any pointer or array declaration nodes until a TypeDecl node
+        is found, retrieving the declname.
+
+        Args:
+            node (Node): The function delcaration type subtree root node.
+
+        Returns:
+            str | None: Returns the function name from the subtree, or None if 
+            the subtree does not contain valid function information. 
+        """
         while isinstance(node, (PtrDecl, ArrayDecl)):
             node = node.type
         if isinstance(node, TypeDecl) and node.declname is not None:
@@ -94,10 +125,61 @@ class FuncArgRandomiserTraverser(NodeVisitor):
         else:
             return None
 
-    def visit_FuncDecl(self, node):
-        # TODO this code could use some major cleaning up!
+    def _mutate_non_empty_func(self, fdecl: FuncDecl, fname: str) -> None:
+        """_summary_
+
+        Args:
+            fdecl (FuncDecl): _description_
+            fname (str): _description_
+        """
+        # Get a list of defined identifiers and arguments
+        defined_idents = self.analyzer.idents
+        args = [
+            arg.name
+            for arg in fdecl.args.params
+            if not isinstance(arg, EllipsisParam)
+        ]
+        
+        # Generate extra spurious arguments - we do not modify ellipsis parameters
+        # (for variadic functions) to ensure correctness - this is kept at the end.
+        extra_args = self.get_extra_args(defined_idents.union(set(args)))
+        before_change = fdecl.args.params.copy()
+        if isinstance(fdecl.args.params[-1], EllipsisParam):
+            ellipsis_arg = fdecl.args.params[-1]
+            fdecl.args.params = fdecl.args.params[:-1] + extra_args
+            if self.randomise:
+                random.shuffle(fdecl.args.params)
+            fdecl.args.params.append(ellipsis_arg)
+        else:
+            fdecl.args.params += extra_args
+            if self.randomise:
+                random.shuffle(fdecl.args.params)
+                
+        # Create a mapping from previous to new argument positions such that
+        # function calls can be appropriately modified. 
+        mapping = {}
+        for i, arg in enumerate(before_change):
+            if isinstance(arg, EllipsisParam):
+                mapping[i] = -1
+            else:
+                mapping[i] = fdecl.args.params.index(arg)
+        self.func_args[fname] = (fdecl.args, mapping)
+
+    def visit_FuncDecl(self, node: FuncDecl) -> None:
+        """Visit a FuncDecl node, recording the function declaration and mutating 
+        it to add spurious arguments and randomise the function argument order. Variadic
+        functions with EllipsisParam nodes are not manipulated. We first check for existing
+        mutations (in the case of function signatures meaning repeats), and if not, any 
+        extra function args are stored in a dictionary such that later signatures or calls
+        can use the same arguments. 
+
+        Args:
+            node (FuncDecl): The FuncDecl node to mutate and traverse.
+        """
         if self.walk_num != 1 or node.type is None:
+            # We only record function declarations on the first walk. 
             return NodeVisitor.generic_visit(self, node)
+        
         defined_idents = self.analyzer.idents
         fdecl = node
         fname = self.get_fname(fdecl.type)
@@ -111,80 +193,69 @@ class FuncArgRandomiserTraverser(NodeVisitor):
                 or fdecl.args.params is None
                 or len(fdecl.args.params) == 0
             ):
-                # For empty functions, create a new ParamList, generate random extra args, and store
+                # For empty functions, we create a new ParamList, generate random 
+                # extra args, and store them.
                 extra_args = self.get_extra_args(defined_idents)
                 fdecl.args = ParamList(extra_args)
                 self.func_args[fname] = (fdecl.args, dict())
             elif (
                 isinstance(fdecl.args.params[0], Typename)
                 and fdecl.args.params[0].type.type.names[0] == "void"
-            ):
-                # TODO check AST for this - might be easier with Typename?
-                # Don't change anything for void functions
+            ): 
+                # Do not change functions with a void parameter.
                 self.func_args[fname] = (fdecl.args, dict())
             else:
-                # For non-empty functions, generate random extra args, randomise order, and store
-                args = [
-                    arg.name
-                    for arg in fdecl.args.params
-                    if not isinstance(arg, EllipsisParam)
-                ]
-                extra_args = self.get_extra_args(defined_idents.union(set(args)))
-                before_change = fdecl.args.params.copy()
-                if isinstance(fdecl.args.params[-1], EllipsisParam):
-                    # TODO this is wrong - all arguments need to be
-                    # in the same order, bogus arguments can be inserted
-                    # at the start but other args need to be in the same
-                    # order otherwise va_args stuff breaks
-                    ellipsis_arg = fdecl.args.params[-1]
-                    fdecl.args.params = fdecl.args.params[:-1] + extra_args
-                    if self.randomise:
-                        random.shuffle(fdecl.args.params)
-                    fdecl.args.params.append(ellipsis_arg)
-                else:
-                    fdecl.args.params += extra_args
-                    if self.randomise:
-                        random.shuffle(fdecl.args.params)
-                mapping = {}
-                for i, arg in enumerate(before_change):
-                    if isinstance(
-                        arg, EllipsisParam
-                    ):  # TODO currently broken on variadic functions
-                        mapping[i] = -1
-                    else:
-                        mapping[i] = fdecl.args.params.index(arg)
-                self.func_args[fname] = (fdecl.args, mapping)
+                # For non-empty functions, generate random args and store
+                self._mutate_non_empty_func(fdecl, fname)
         else:
             fdecl.args = self.func_args[fname][0]
         NodeVisitor.generic_visit(self, node)
 
-    def get_random_val(
-        self, node
-    ):  # TODO currently only supports a constant option - should be option to only randomise
-        # using variables in the program, wherever possible - very sick idea!
+    def get_random_val(self, node: Node) -> Constant:
+        """Given the AST subtree root for a parameter, this function will
+        generate a random constant value fitting that parameter such that
+        function calls can be modified to fit spurious arguments. 
+
+        Args:
+            node (Node): The parameter AST subtree root
+
+        Returns:
+            Constant: A random constant value of the same type as that parameter.
+        """
         return self.types[node.type.type.names[0]]()
 
-    def visit_FuncCall(self, node):
+    def visit_FuncCall(self, node: FuncCall) -> None:
+        """Visit a FuncCall AST node, mutating the function call such that arguments 
+        are moved to match their new randomised order, and constant values of the correct
+        type are inserted to match extra inserted spurious arguments. 
+
+        Args:
+            node (FuncCall): The FuncCall AST node to traverse and mutate.
+        """
         fname = node.name.name
         if self.walk_num == 1 or fname not in self.func_args:
+            # We only mutate on the second walk of the abstract syntax tree (AST)
             return NodeVisitor.generic_visit(self, node)
         if node.args is None and self.extra > 0:
-            node.args = ExprList([]) 
-            # Convert empty function calls
+            node.args = ExprList([]) # Convert empty func calls
         elif node.args is None and self.extra == 0:
             return NodeVisitor.generic_visit(self, node)
+    
+        # Retrieve the stored function specification mapping
         new_args, mapping = self.func_args[fname]
         first_arg = new_args.params[0].type.type
         if isinstance(first_arg, IdentifierType) and first_arg.names[0] == "void":
             return NodeVisitor.generic_visit(self, node)
         call_args = [None] * (len(node.args.exprs) + self.extra)
         for before, after in mapping.items():
-            if after == -1:  # Ellipsis Param (variadic function)
+            if after == -1:  # Handle Ellipsis Param (variadic function)
                 for i in range(len(node.args.exprs) - 1, before - 1, -1):
                     call_args[after] = node.args.exprs[i]
                     after -= 1
             else:
                 call_args[after] = node.args.exprs[before]
+        
+        # Generate spurious function arguments 
         for i, arg in enumerate(call_args):
             if arg is not None:
                 continue
@@ -192,17 +263,32 @@ class FuncArgRandomiserTraverser(NodeVisitor):
         node.args.exprs = call_args
         NodeVisitor.generic_visit(self, node)
 
-    def visit_FileAST(self, node):
+    def visit_FileAST(self, node: FileAST) -> None:
+        """Visit a FileAST node (the abstracy syntax tree root), performing two walks
+        of the AST. The first walk is to find and mutate all possible function signatures,
+        whereas the second walk is to correspondingly mutate all function calls; this is
+        needed because pycparser doesn't store another information for just function
+        signatures alone, and hence their usage can break single-traversal functionality.
+
+        Args:
+            node (FileAST): The FileAST node to traverse.
+        """
         NodeVisitor.generic_visit(self, node)
         self.walk_num += 1
         NodeVisitor.generic_visit(self, node)
         self.reset()
         
     def transform(self, source: interaction.CSource) -> None:
+        """Transform the given C source program, by first performing identifier usage
+        analysis (to get a list of defined identifiers to avoid shadowing their names) and
+        then traversing the AST. 
+
+        Args:
+            source (interaction.CSource): The C source program to transform.
+        """
         self.analyzer.load(source)
         self.analyzer.process()
         self.visit(source.t_unit)
-        
 
 
 class FuncArgumentRandomiseUnit(ObfuscationUnit):
@@ -223,11 +309,25 @@ class FuncArgumentRandomiseUnit(ObfuscationUnit):
     type = TransformType.PROCEDURAL
 
     def __init__(self, extra_args: int, randomise: bool):
+        """The constructor for the FuncArgumentRandomiseUnit transformation.
+
+        Args:
+            extra_args (int): The number of extra spurious arguments to insert.
+            randomise (bool): Whether to randomise argument orders or not.
+        """
         self.extra_args = extra_args
         self.randomise = randomise
         self.traverser = FuncArgRandomiserTraverser(extra_args, randomise)
 
     def transform(self, source: interaction.CSource) -> interaction.CSource:
+        """Performs the function interface randomisation transformation on the given source program.
+
+        Args:
+            source (interaction.CSource): The source code to transform.
+
+        Returns:
+            interaction.CSource: The transformed source code.
+        """
         self.traverser.transform(source)
         new_contents = generate_new_contents(source)
         return interaction.CSource(source.fpath, new_contents, source.t_unit)
@@ -303,6 +403,11 @@ class FuncArgumentRandomiseUnit(ObfuscationUnit):
         return FuncArgumentRandomiseUnit(json_obj["extra_args"], json_obj["randomise"])
 
     def __str__(self) -> str:
+        """Create a readable string representation of the FuncArgRandomiseUnit.
+
+        Returns:
+            str: The readable string representation.
+        """
         extra_args_flag = f"extra={self.extra_args}"
         randomise_flag = f"random_order={'ENABLED' if self.randomise else 'DISABLED'}"
         return f"RandomiseFuncArgs({extra_args_flag},{randomise_flag})"
