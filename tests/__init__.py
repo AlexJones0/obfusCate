e452b69735590b7b6eb7789c82f407ae2292c136
xeovz <65985521+xeovz@users.noreply.github.com>
2023-04-24 03:11:24 +0100

    Fix test config file paths for Linux/Mac

--- tests/__init__.py
+++ tests/__init__.py
@@ -15,9 +15,9 @@ def clean_dir() -> None:
 def reset_config() -> None:
     """Resets the state of the config used during testing to a default state,
     such that the config can be re-used between tests without mutation of state."""
-    cfg.LOG_PATH = ".\\logs\\"
-    cfg.COMP_PATH = ".\\compositions\\auto\\"
-    cfg.TEMP_FILE_PATH = ".\\obfuscate_temp.c"
+    cfg.LOG_PATH = "./logs/"
+    cfg.COMP_PATH = "./compositions/auto/"
+    cfg.TEMP_FILE_PATH = "./obfuscate_temp.c"
     cfg.LOG_FILE = ""
     cfg.LOGS_ENABLED = True
     cfg.CALCULATE_COMPLEXITY = True
